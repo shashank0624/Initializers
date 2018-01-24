86e3cae53c41d2a869613358de2bb5fc9b063bba
Shashank Panwar <shashankpanwar4@gmail.com>
2018-01-24 19:13:23 +0530

    Working with date

---
+++ Initializers/File.swift
@@ -0,0 +1,45 @@
+//
+//  File.swift
+//  Initializers
+//
+//  Created by Shashank Panwar on 1/24/18.
+//  Copyright © 2018 Shashank Panwar. All rights reserved.
+//
+
+import Foundation
+
+class Animal{
+    
+    init(){
+        
+    }
+}
+
+
+class Person: Animal{
+    
+    var name: String
+    var age: Int
+    var dateOfBirth: NSDate
+    
+    init(age: Int, name: String, dateOfBirth: NSDate) {
+        self.age = age
+        self.name = name
+        self.dateOfBirth = dateOfBirth
+        super.init()
+    }
+    
+    convenience init(name: String, dateOfBirth:NSDate){
+        
+        let calender = Calendar.current
+        
+        let ageComponent = calender.dateComponents([.year], from: dateOfBirth as Date, to: Date())
+        
+        let age = ageComponent.year
+        
+        print("Age :\(age)")
+        self.init(age: age!, name: name, dateOfBirth: dateOfBirth)
+        
+    }
+    
+}
--- Initializers/ViewController.swift
+++ Initializers/ViewController.swift
@@ -12,14 +12,40 @@ class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view, typically from a nib.
+        
+       let now = Date()
+        print("Current Date: \(now)")
+        
+        let TwentyMinutesAfter = Date(timeIntervalSinceNow: 20*60)
+        print("Twenty Minutes After time will be \(TwentyMinutesAfter)")
+        
+        let TwentyMinutesAgo = Date(timeIntervalSinceNow: -20*60)
+        print("Twenty Minutes Before time was \(TwentyMinutesAgo)")
+        
+        let referenceTo1970 = Date(timeIntervalSince1970: 20*60)
+        print("Reference to 1970 is \(referenceTo1970)")
+        
+        let oneYear = TimeInterval(60*60*24*365)
+        let newYear1971 = Date(timeIntervalSince1970: oneYear)
+        print("After one year time will be \(newYear1971)")
+        
+        let calender = Calendar(identifier: .gregorian)
+        var birthDateComponent = DateComponents()
+        birthDateComponent.year = 1989
+        birthDateComponent.month = 11
+        birthDateComponent.day = 6
+        birthDateComponent.timeZone = TimeZone(abbreviation: "UTC")
+        
+        let birthDate = calender.date(from: birthDateComponent)
+        
+        print("BirthDate: \(birthDate)")
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd-MM-yyyy HH:mm"
+        let dateInFormat = dateFormatter.string(from: Date())
+        print("Formatted Date: \(dateInFormat)")
     }
-
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
-    }
-
 
 }
+
 
